47bd91e579f05594f7c66450451b4b8af0d11195
iamjesam <jmetro181@gmail.com>
2026-05-06 21:58:25 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Switch, Route, Link} from 'react-router-dom';
+import { Layout, Typogaphy, Space } from 'antd'
+import { Navbar } from './components'
+
+function App() {
+  return (
+  <div className='app'>
+    <div className='navbar'>
+       <Navbar />
+    </div>
+    <div className='navbar'>
+      
+    </div>
+    <div className='navbar'>
+      
+    </div>
+  </div>
+  )
+};
+
+export default App;
