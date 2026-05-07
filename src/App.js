9a35570322b36bfc95669ce61d11661f1cb0f884
iamjesam <jmetro181@gmail.com>
2026-05-07 13:55:38 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Switch, Route, Link} from 'react-router-dom';
+/*import { Switch, Route, Link} from 'react-router-dom';*/
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typogaphy, Space } from 'antd'
-import { Navbar } from './components'
+import { Navbar, Exchanges, Homepage, CryptoDetails, CryptoCurrencies, News } from './components'
 import './App.css'
+
 
 function App() {
   return (
@@ -10,8 +12,28 @@ function App() {
     <div className='navbar'>
        <Navbar />
     </div>
-    <div className='navbar'>
-      
+    <div className='main'>
+      <Layout>
+        <div className='routes'>
+          <Routes>
+            <Route exact path='/' element={<Homepage />}>
+             
+            </Route>
+            <Route exact path='/exchanges' element={<Exchanges />}>
+             
+            </Route>
+            <Route exact path='/cryptocurrencies' element={<CryptoCurrencies/>}>
+               
+            </Route>
+            <Route exact path='/crypto/:coinId' element={ <CryptoDetails />}>
+              
+            </Route>
+            <Route exact path='/news' element={<News />}>
+               
+            </Route>
+          </Routes>
+        </div>
+      </Layout>
     </div>
     <div className='navbar'>
       
