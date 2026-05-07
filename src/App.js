f384600b5056b5c5d792794301d3c5a079d89f5c
iamjesam <jmetro181@gmail.com>
2026-05-07 11:09:50 +0100

    App.css commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route, Link} from 'react-router-dom';
 import { Layout, Typogaphy, Space } from 'antd'
 import { Navbar } from './components'
+import './App.css'
 
 function App() {
   return (
