1f8145cdacc0c926ceec53c3218d8bf8e3821037
iamjesam <jmetro181@gmail.com>
2026-05-11 21:50:30 +0100

    Service folder

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-key': 'd07db0b81bmshedbef85b36c1306p1e2aafjsn018074d0a1fe',
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi ({
+    reducerPath: "cryptoApi",
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
