651c8c08410bd0dd30fc845b659baba538785658
iamjesam <jmetro181@gmail.com>
2026-05-14 23:43:40 +0100

    Commit to repo

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -1,18 +1,51 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
+import millify from 'millify';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const CryptoCurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
-  const { cryptos, setCryptos} = useState(cryptosList?.data?.coins);
+const CryptoCurrencies = ({simplified}) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  //const [ cryptos, setCryptos ] = useState(cryptosList?.data?.coins);
 
-  console.log(cryptos);
+  const [cryptos, setCryptos] = useState([]);
+
+  useEffect(() => {
+    setCryptos(cryptosList?.data?.coins);
+  }, [cryptosList]);
+
+  if (isFetching) return 'Loading...';
+
   return (
-    <div>
-      <h1>hdddh</h1>
-    </div>
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={currency.uuid}
+          >
+
+            {/* Note: Change currency.id to currency.uuid  */}
+            <Link  to={`/crypto/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} alt={currency.name} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {currency.change}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
